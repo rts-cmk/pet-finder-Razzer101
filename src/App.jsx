a55dc6a644aa7490afc48f3b9bd6449860e6c2fe
Razzer101 <Rasmus@anders-net.dk>
2025-11-17 09:14:46 +0100

    chore: React-Vite-ReactRouter-Sass setup with the files needed

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router'
+import './App.css'
+import Home from './pages/Home'
+import Details from './pages/Details'
+import Error from './pages/Error'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route index element={<Home/>}/>
+      <Route path="details/:petId" element={<Details/>}/>
+      <Route path="*" element={<Error/>}/>
+    </Routes>
+  )
+}
+
+export default App
