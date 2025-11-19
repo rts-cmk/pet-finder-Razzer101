e0268781f53c8bd79a95747bb7068bbc7ccb7e43
Razzer101 <Rasmus@anders-net.dk>
2025-11-19 12:31:26 +0100

    fix: Changed pet breed/names to be cut if longer than 11 characters

--- src/components/PetSection.jsx
+++ src/components/PetSection.jsx
@@ -21,7 +21,7 @@ export default function PetSection({ petData }){
                                 <img className="pet-item__img" src={elm.image} alt={elm.breed} />
                                 <article className="pet-info">
                                     <div className="pet-header">
-                                        <h2 className="pet-header__title">{elm.breed}</h2>
+                                        <h2 className="pet-header__title">{elm.breed.slice(0, 11)}{elm.breed.length > 11 ? "..." : ""}</h2>
                                     </div>
                                     <p className="pet-info__location"><LuMapPin />{elm.location}</p>
                                     <p className="pet-info__description">{elm.short_description.slice(0, 51)}...</p>
